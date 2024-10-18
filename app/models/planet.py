b1c8a920b175470b46b42cf4bb8049b395cad163
vlada <vladarap88@gmail.com>
2024-10-18 11:53:58 -0700

    created structure  new files models/plant and routes/routes and class Planet

---
+++ app/models/planet.py
@@ -0,0 +1,7 @@
+class Planet:
+    def __init__(self, id, name, description, distance_from_sun, flag):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.distance_from_sun = distance_from_sun
+        self.flag = flag
