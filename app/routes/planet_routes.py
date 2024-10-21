c219d7f88ffdddf7b2eba10ba41c42550c970ddc
vlada <vladarap88@gmail.com>
2024-10-21 11:40:40 -0700

    worked together with live share to create endpoints of one planet and error hendaling

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,24 +1,51 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planet_models import planets_list
 
 
-planets_bp = Blueprint("planets_bp", __name__, url_prefix ="/planets")
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
 
 @planets_bp.get("")
-
 def get_all_planets():
 
     results_list = []
 
     for planet in planets_list:
-        results_list.append(dict(
-            id=planet.id,
-            name=planet.name,
-            description=planet.description,
-            distance_from_sun=planet.distance_from_sun
-        ))
+        results_list.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "distance_from_sun": planet.distance_from_sun,
+            }
+        )
 
     return results_list
 
 
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
 
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "distance_from_sun": planet.distance_from_sun,
+    }
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+
+    except ValueError:
+        response = {"message": f"Planet {planet_id} invalid"}
+        abort(make_response(response), 400)
+
+    for planet in planets_list:
+        if planet.id == planet_id:
+            return planet
+
+    response = {"message": f"Planet {planet_id} not found"}
+    abort(make_response(response), 404)
