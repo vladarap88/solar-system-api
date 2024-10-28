62f63a33ec9c769a1eb59443a2f37d5cdbeaeaaf
Aleida Vieyra <avieyra@ufl.edu>
2024-10-28 15:54:10 -0400

    added the configurations for create_app() inside __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from .db import db, migrate
-from app.models import planet
+from .routes.db import db, migrate
+from .models import planet
 from .routes.planet_routes import planets_bp
 
-
-# def create_app(test_config=None):
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
     app.register_blueprint(planets_bp)
 
     return app
