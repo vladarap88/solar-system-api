6668986136cdf9dafedbb1049c69dec62f0124e0
vlada <vladarap88@gmail.com>
2024-10-31 14:03:04 -0700

    updated distance from sun in seed.py

--- seed.py
+++ seed.py
@@ -7,56 +7,56 @@ with team_app.app_context():
         Planet(
             name="Mercury",
             description="The smallest and closest planet to the Sun",
-            distance_from_sun=36,
+            distance_from_sun=36000000,
         )
     ),
     db.session.add(
         Planet(
             name="Venus",
             description="Similar in size to Earth",
-            distance_from_sun=67,
+            distance_from_sun=67000000,
         )
     ),
     db.session.add(
         Planet(
             name="Earth",
             description="The only known planet to support life",
-            distance_from_sun=93,
+            distance_from_sun=93000000,
         )
     ),
     db.session.add(
         Planet(
             name="Mars",
             description="Known as the Red Planet",
-            distance_from_sun=142,
+            distance_from_sun=142000000,
         )
     ),
     db.session.add(
         Planet(
             name="Jupiter",
             description="The largest planet in the solar system",
-            distance_from_sun=484,
+            distance_from_sun=484000000,
         )
     ),
     db.session.add(
         Planet(
             name="Saturn",
             description="Famous for its beautiful ring system",
-            distance_from_sun=886,
+            distance_from_sun=886000000,
         )
     ),
     db.session.add(
         Planet(
             name="Uranus",
             description="An ice giant with a blue-green hue",
-            distance_from_sun=1780,
+            distance_from_sun=1780000000,
         )
     ),
     db.session.add(
         Planet(
             name="Neptune",
             description="The farthest planet from the Sun",
-            distance_from_sun=2800,
+            distance_from_sun=2800000000,
         )
     ),
     db.session.commit()
