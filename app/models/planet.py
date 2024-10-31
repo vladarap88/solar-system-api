d0832a299e9fed775abbbde53e8e6b1bccfd52b4
vlada <vladarap88@gmail.com>
2024-10-31 12:02:50 -0700

    changed seed.py and deleted migrations file and import decimal into distance from sun

--- app/models/planet.py
+++ app/models/planet.py
@@ -21,24 +21,19 @@
 
 from sqlalchemy.orm import Mapped, mapped_column
 from app.routes.db import db
+from sqlalchemy import decimal
+
 
 class Planet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
     description: Mapped[str]
-    distance_from_sun: Mapped[int]
+    distance_from_sun: Mapped[decimal.Decimal]
 
     def to_dict(self):
         return dict(
             id=self.id,
             name=self.name,
             description=self.description,
-            distance_from_sun=self.distance_from_sun
+            distance_from_sun=self.distance_from_sun,
         )
-
-
-
-    
-
-
-
