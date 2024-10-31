696dd56bba99764e6d286468d7f5456615c562dc
vlada <vladarap88@gmail.com>
2024-10-31 12:37:44 -0700

    added migrate and changed seed.py the distance from sun

--- app/models/planet.py
+++ app/models/planet.py
@@ -21,14 +21,16 @@
 
 from sqlalchemy.orm import Mapped, mapped_column
 from app.routes.db import db
-from sqlalchemy import decimal
+
+
+
 
 
 class Planet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
     description: Mapped[str]
-    distance_from_sun: Mapped[decimal.Decimal]
+    distance_from_sun: Mapped[int]
 
     def to_dict(self):
         return dict(
--- seed.py
+++ seed.py
@@ -7,56 +7,56 @@ with team_app.app_context():
         Planet(
             name="Mercury",
             description="The smallest and closest planet to the Sun",
-            distance_from_sun=36000000,
+            distance_from_sun=36,
         )
     ),
     db.session.add(
         Planet(
             name="Venus",
             description="Similar in size to Earth",
-            distance_from_sun=67000000,
+            distance_from_sun=67,
         )
     ),
     db.session.add(
         Planet(
             name="Earth",
             description="The only known planet to support life",
-            distance_from_sun=93000000,
+            distance_from_sun=93,
         )
     ),
     db.session.add(
         Planet(
             name="Mars",
             description="Known as the Red Planet",
-            distance_from_sun=142000000,
+            distance_from_sun=142,
         )
     ),
     db.session.add(
         Planet(
             name="Jupiter",
             description="The largest planet in the solar system",
-            distance_from_sun=4840000000,
+            distance_from_sun=484,
         )
     ),
     db.session.add(
         Planet(
             name="Saturn",
             description="Famous for its beautiful ring system",
-            distance_from_sun=886000000,
+            distance_from_sun=886,
         )
     ),
     db.session.add(
         Planet(
             name="Uranus",
             description="An ice giant with a blue-green hue",
-            distance_from_sun=1780000000,
+            distance_from_sun=1780,
         )
     ),
     db.session.add(
         Planet(
             name="Neptune",
             description="The farthest planet from the Sun",
-            distance_from_sun=2800000000,
+            distance_from_sun=2800,
         )
     ),
     db.session.commit()
