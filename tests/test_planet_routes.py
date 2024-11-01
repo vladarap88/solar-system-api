7d245f4b87fc3a1e4228932f21775531f9d1688a
vlada <vladarap88@gmail.com>
2024-10-31 22:08:57 -0700

    fix formatting

--- tests/test_planet_routes.py
+++ tests/test_planet_routes.py
@@ -1,43 +1,48 @@
 def test_get_all_planets_with_no_records(client):
-    #Act
+    # Act
     response = client.get("/planets")
     response_body = response.get_json()
 
-    #Assert
+    # Assert
     assert response.status_code == 200
     assert response_body == []
 
+
 def test_get_one_planet_succeeds(client, two_saved_planets):
     response = client.get("planets/1")
-    response_body= response.get_json()
+    response_body = response.get_json()
 
     assert response.status_code == 200
     assert response_body == {
         "id": 1,
-        "name":"Mercury",
+        "name": "Mercury",
         "description": "The smallest and closest planet to the Sun",
-        "distance_from_sun": 36
+        "distance_from_sun": 36,
     }
 
-def test_nonexistent_planet_return_404(client):
-     response = client.get("planets/1")
-     response_body= response.get_json()
 
-     assert response.status_code == 404
+def test_nonexistent_planet_return_404(client):
+    response = client.get("planets/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
 
 
 def test_create_one_planet_in_empty_db(client):
-    response = client.post("/planets", json={
-        "name":"Pluto",
-        "description": "Previously our 9th planet, is now classified as a dwarf planet.",
-        "distance_from_sun": 3700
-    })
+    response = client.post(
+        "/planets",
+        json={
+            "name": "Pluto",
+            "description": "Previously our 9th planet, is now classified as a dwarf planet.",
+            "distance_from_sun": 3700,
+        },
+    )
     response_body = response.get_json()
 
     assert response.status_code == 201
     assert response_body == {
         "id": 1,
-        "name":"Pluto",
+        "name": "Pluto",
         "description": "Previously our 9th planet, is now classified as a dwarf planet.",
-        "distance_from_sun": 3700
+        "distance_from_sun": 3700,
     }
